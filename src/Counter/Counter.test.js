23caedd243948f139a69ba5f3b157f39deaedfa8
Bernard Munyiri <bernard.munyiri@gmail.com>
2023-01-20 16:38:22 +0300

    added all the test elements that are to be tested as we create the counter App

--- src/Counter/Counter.test.js
+++ src/Counter/Counter.test.js
@@ -2,7 +2,31 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import Counter from './Counter';
 
-it('renders welcome message', () => {
+it('renders the component header', () => {
   render(<Counter />);
   expect(screen.getByText('Universal Counter')).toBeInTheDocument();
 });
+
+it( 'renders the initial text of 0', () => {
+    
+    render( <Counter /> );
+    expect(screen.getByTextId("counter")).toBe('0')
+} )
+
+it( "render s the initial input value of '1'", () => {
+    
+    render( <Counter /> );
+    expect(screen.queryAllByDisplayValue("1")).toBe("1")
+} )
+
+Text( "renders the addtion button with +", () => {
+    
+    render( <Counter /> )
+     expect(screen.getByTextId("add")).toBe('+')
+} )
+
+Text( "renders the subtraction button with -", () => {
+    
+    render( <Counter /> )
+     expect(screen.getByTextId("subtract")).toBe('-')
+} )
